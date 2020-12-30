5b182f7c263fe70e6f95a2be88193b8089f259e8
Muhammad Rizal Firdaus <rizal.firdaus117@gmail.com>
2020-12-30 09:13:41 +0700

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 5f;
+    private PlayerMotor motor;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+
+
+
+    // memanggil komponen saat game dijalankan
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        //mendeteksi input vertical dan horizontal
+        float _xMove = Input.GetAxis("Horizontal");
+        float _zMove = Input.GetAxis("Vertical");
+
+        //membuat velocity dan lakukan normalize dikalikan dengan speed player
+        Vector3 _movHoriz = transform.right * _xMove;
+        Vector3 _movVerti = transform.forward * _zMove;
+        Vector3 _velocity = (_movHoriz + _movVerti) * speed;
+        motor.Move(_velocity);
+
+        //untuk deteksi input mouse
+        float _yBot = Input.GetAxis("Mouse X");
+        Vector3 _rotation = new Vector3(0f, _yBot, 0f) * lookSensitivity;
+        motor.Rotate(_rotation);
+
+
+        float _xBot = Input.GetAxis("Mouse Y");
+        float _cameraRotationX = _xBot * lookSensitivity;
+        motor.RotateCamera(_cameraRotationX);
+
+    }
+}
---
+++ PlayerMotor.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    //rotate x limit
+    private float rotateCameraX = 0f;
+    private float currentRotateCameraX = 0f;
+
+    [SerializeField]
+    private float camRotateLimit = 85f;
+    private Rigidbody rb;
+
+    // include rigidbody
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //pass variable
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(float _rotateCameraX)
+    {
+        rotateCameraX = _rotateCameraX;
+    }
+
+
+    //memanggil pergerakan dan rotasi 
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+           rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+
+        if (cam != null)
+        {
+
+            // set rotasi dan clamp it
+            currentRotateCameraX -= rotateCameraX;
+            currentRotateCameraX = Mathf.Clamp(currentRotateCameraX, -camRotateLimit, camRotateLimit);
+            // apply rotation to our cam
+            cam.transform.localEulerAngles = new Vector3(currentRotateCameraX, 0f, 0f);
+
+        }
+    }
+}
